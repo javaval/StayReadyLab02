8f53c325e6df2f6abfdb243821516edd9479fd27
Valerie Andy <65504047+javaval@users.noreply.github.com>
2020-06-24 14:43:36 -0400

    Update Test files

---
+++ src/test/java/com/codedifferently/DisplayModeTest.java
@@ -0,0 +1,10 @@
+package com.codedifferently;
+
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class DisplayModeTest {
+    
+}
---
+++ src/test/java/com/codedifferently/TrigUnitsTest.java
@@ -0,0 +1,8 @@
+package com.codedifferently;
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TrigUnitsTest {
+    
+}
